ec30e8afa68f66fbbf6c7a4bcf37c3e9bf347045
Parwej <ahmar.parwej@capgemini.com>
2021-11-04 22:20:25 -0400

    Removed unused code

--- service/src/main/java/com/marionete/services/service/UserAccountServiceImpl.java
+++ service/src/main/java/com/marionete/services/service/UserAccountServiceImpl.java
@@ -25,34 +25,23 @@ public class UserAccountServiceImpl implements UserAccountService {
 
 	@Autowired
 	private AccountInfoRepository accountInfoRepository;
-	
-	@Autowired
-	Executor serviceExecutor;
 
 	@Override
 	public UserAccount getUserAccount(String loginToken) {
 
 		UserAccount userAccount = new UserAccount();
-		
-		ExecutorService serviceExecutor= Executors.newFixedThreadPool(2);
-		
+
 		CompletableFuture<AccountInfo> accountInfoFuture = CompletableFuture.supplyAsync(()->{
-			//userAccount.setAccountInfo(accountInfoRepository.getAccountInfo(loginToken));
 			return accountInfoRepository.getAccountInfo(loginToken);
 		});
 		
 		CompletableFuture<UserInfo> userAccountFuture = CompletableFuture.supplyAsync(()->{
 			return userAccountRepository.getUserAccount(loginToken);
 		});
-		
-		//CompletableFuture<Void> allFuture = CompletableFuture.allOf(accountInfoFuture,userAccountFuture);
-		//serviceExecutor.submit(accountInfoFuture);
+
 		try {
-			serviceExecutor.shutdown();
 			userAccount.setAccountInfo(accountInfoFuture.get());
 			userAccount.setUserInfo(userAccountFuture.get());
-			//allFuture.get();
-			serviceExecutor.awaitTermination(30, TimeUnit.SECONDS);
 		} catch (InterruptedException|ExecutionException e) {
 			System.out.println("Failed to Complete service request");
 			e.printStackTrace(System.err);
